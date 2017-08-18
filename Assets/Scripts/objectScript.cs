bca2f844fa0a545cf0fc462b665071c99b2c2bce
Tory <vickasvetlova@gmail.com>
2017-08-18 15:37:58 +0300

    add inside
    inside

---
+++ Assets/Scripts/objectScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectScript : MonoBehaviour {
+    private bool inside=false;
+	public void LookAT()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+    public void Inside()
+    {
+        if (!inside)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+        inside = !inside;
+    }
+   
+}
