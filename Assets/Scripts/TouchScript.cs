a25501bb14657329a5b6931c075aba9e6efbf55b
Anna Sidorova <xbitstream@gmail.com>
2017-08-22 00:49:17 +0300

    Alpha
    Alpha version 0.1

--- Assets/Scripts/TouchScript.cs
+++ Assets/Scripts/TouchScript.cs
@@ -17,6 +17,13 @@ public class TouchScript : MonoBehaviour
     public Text text;
     public bool setPos;
 
+    private GameObject tempObjec = null;
+
+    private float rotationSpeed = 1f;
+    private Vector3 initTouch;
+    private float rotX;
+    private float rotY;
+
     private void Start()
     {
         Stat = state.move;
@@ -40,6 +47,13 @@ public class TouchScript : MonoBehaviour
         }
         text.text = Stat.ToString();
     }
+
+    public void changeSens(float sens){
+        rotationSpeed = sens;
+        text.text = ""+rotationSpeed;
+
+    }
+
 
     void Update()
     {
@@ -67,14 +81,12 @@ public class TouchScript : MonoBehaviour
 
                         if (Input.GetMouseButtonDown(0))
                         {
-                            Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+                           
                             Debug.Log("pressDown");
                             dist = hit.distance; //hit.distance + Camera.main.nearClipPlane;
                             trim = hit.collider.transform.position - hit.point;
-
                             recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-                            //recipient.GetComponent<Booton>().touchPosition = hit.point;
+                           
                         }
                         if (Input.GetMouseButtonUp(0))
                         {
@@ -103,8 +115,59 @@ public class TouchScript : MonoBehaviour
                     }
                 }
                 break;
-            case state.rotation:
 
+            //поворот
+            case state.rotation:
+                if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+
+                {
+                    touchesOld = new GameObject[touchList.Count];
+                    touchList.CopyTo(touchesOld);
+                    touchList.Clear();
+
+
+
+
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+
+                    if (Physics.Raycast(ray, out hit, touchInputMask))
+                    {
+                        GameObject recipient = hit.transform.gameObject;
+                        touchList.Add(recipient);
+
+
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                            tempObjec = recipient;
+                        }
+                        if (Input.GetMouseButtonUp(0))
+                        {
+                            tempObjec = null;
+                            recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
+
+                        }
+                        if (tempObjec && Input.GetMouseButton(0))
+                        {
+
+                            recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                            float rotX = Input.GetAxis(("Mouse X")) * rotationSpeed;
+                            float rotY = Input.GetAxis(("Mouse Y")) * rotationSpeed;
+                            tempObjec.transform.Rotate(Vector3.up, -rotX, Space.World);
+							tempObjec.transform.Rotate(Vector3.up, -rotY, Space.World);
+
+						}
+                    }
+                    foreach (var g in touchesOld)
+                    {
+
+                        if (!touchList.Contains(g))
+                        {
+                            g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                        }
+                    }
+                }
 
                 break;
         }
@@ -113,56 +176,120 @@ public class TouchScript : MonoBehaviour
 
 #endif
 
-
-        if (Input.touchCount > 0)
+        switch (Stat)
         {
-            touchesOld = new GameObject[touchList.Count];
-            touchList.CopyTo(touchesOld);
-            touchList.Clear();
 
-            foreach (var touch in Input.touches)
-            {
+            case state.move:
 
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, touchInputMask))
+                if (Input.touchCount > 0)
                 {
-                    GameObject recipient = hit.transform.gameObject;
-                    touchList.Add(recipient);
-                    if (touch.phase == TouchPhase.Began)
-                    {
-                        dist = hit.distance;
-                        trim = hit.collider.transform.position - hit.point;
-                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-                        //  recipient.GetComponent<Booton>().touchPosition = touch.position;
-                    }
-                    if (touch.phase == TouchPhase.Ended)
-                    {
-                        recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
-                    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-                    {
-                        Vector3 pos = touch.position;
-                        pos.z = dist;
-                        pos = Camera.main.ScreenToWorldPoint(pos) + trim; // это новая координата
-                        recipient.GetComponent<Booton>().touchPosition = pos;
-                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    touchesOld = new GameObject[touchList.Count];
+                    touchList.CopyTo(touchesOld);
+                    touchList.Clear();
 
-                    }
-                    if (touch.phase == TouchPhase.Canceled)
+                    foreach (var touch in Input.touches)
                     {
-                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
-                    }
-                }
-            }
-            foreach (var g in touchesOld)
-            {
-                if (!touchList.Contains(g))
-                {
-                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
-                }
-            }
 
+                        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, touchInputMask))
+                        {
+                            GameObject recipient = hit.transform.gameObject;
+                            touchList.Add(recipient);
+                            if (touch.phase == TouchPhase.Began)
+                            {
+                                dist = hit.distance;
+                                trim = hit.collider.transform.position - hit.point;
+                                recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                                //  recipient.GetComponent<Booton>().touchPosition = touch.position;
+                            }
+                            if (touch.phase == TouchPhase.Ended)
+                            {
+                                recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
+                            }
+                            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+                            {
+                                Vector3 pos = touch.position;
+                                pos.z = dist;
+                                pos = Camera.main.ScreenToWorldPoint(pos) + trim; // это новая координата
+                                recipient.GetComponent<Booton>().touchPosition = pos;
+                                recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+
+                            }
+                            if (touch.phase == TouchPhase.Canceled)
+                            {
+                                recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                            }
+                        }
+                    }
+                    foreach (var g in touchesOld)
+                    {
+                        if (!touchList.Contains(g))
+                        {
+                            g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                        }
+                    }
+
+                }
+                break;
+            case state.rotation:
+
+				if (Input.touchCount > 0)
+				{
+					touchesOld = new GameObject[touchList.Count];
+					touchList.CopyTo(touchesOld);
+					touchList.Clear();
+
+					foreach (var touch in Input.touches)
+					{
+
+						Ray ray = Camera.main.ScreenPointToRay(touch.position);
+						RaycastHit hit;
+						if (Physics.Raycast(ray, out hit, touchInputMask))
+						{
+							GameObject recipient = hit.transform.gameObject;
+							touchList.Add(recipient);
+							if (touch.phase == TouchPhase.Began)
+							{
+								tempObjec = recipient;                              
+								recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+								
+							}
+							if (touch.phase == TouchPhase.Ended)
+							{
+								recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
+                                tempObjec = null;
+
+
+							}
+                            if (tempObjec && touch.phase == TouchPhase.Moved)
+							{
+                                recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+
+                                float rotX = touch.deltaPosition.x * rotationSpeed;
+								float rotY = touch.deltaPosition.y * rotationSpeed;
+                                tempObjec.transform.Rotate(Vector3.up, -rotX,Space.World);
+                                tempObjec.transform.Rotate(Vector3.up, -rotY,Space.World);
+                                                      
+
+							}
+							if (touch.phase == TouchPhase.Canceled)
+							{
+								recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                               // tempObjec = null;
+							}
+						}
+					}
+					foreach (var g in touchesOld)
+					{
+						if (!touchList.Contains(g))
+						{
+							g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+						}
+					}
+
+				}
+                break;
         }
     }
 }
