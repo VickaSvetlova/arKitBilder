41dbcf2d28a8ae3b67c840e33eb32966f5fc09f1
Tory <vickasvetlova@gmail.com>
2017-08-21 18:06:27 +0300

    add switch state

--- Assets/Scripts/TouchScript.cs
+++ Assets/Scripts/TouchScript.cs
@@ -12,7 +12,7 @@ public class TouchScript : MonoBehaviour
     private RaycastHit hit;
     private Vector3 trim;
     private float dist;
-    private enum state { move,rotation,setpoint}
+    private enum state { move, rotation, setpoint }
     private state Stat;
     public Text text;
     public bool setPos;
@@ -47,67 +47,73 @@ public class TouchScript : MonoBehaviour
 
         switch (Stat)
         {
-            case state.move:               
+            case state.move:
 
-        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+                if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
 
-        {
-            touchesOld = new GameObject[touchList.Count];
-            touchList.CopyTo(touchesOld);
-            touchList.Clear();
-
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
-
-            if (Physics.Raycast(ray, out hit, touchInputMask))
-            {
-                GameObject recipient = hit.transform.gameObject;
-                drawLine(ray, recipient.transform.position);
-                touchList.Add(recipient);
-
-                if (Input.GetMouseButtonDown(0))
                 {
-                    Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    touchesOld = new GameObject[touchList.Count];
+                    touchList.CopyTo(touchesOld);
+                    touchList.Clear();
 
-                    Debug.Log("pressDown");
-                    dist = hit.distance; //hit.distance + Camera.main.nearClipPlane;
-                    trim = hit.collider.transform.position - hit.point;
 
-                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
-                    //recipient.GetComponent<Booton>().touchPosition = hit.point;
-                }
-                if (Input.GetMouseButtonUp(0))
-                {
-                    recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
-                }
-                if (Input.GetMouseButton(0))
-                {
-                    Debug.Log("mouseButton");
-                    Vector3 pos = Input.mousePosition;
-                    pos.z = dist;
-                    //pos = ray.origin + ray.direction * dist;
-                    //pos = ray.direction+ trim;
-                    pos = Camera.main.ScreenToWorldPoint(pos) + trim; // это новая координата
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
 
-                    recipient.GetComponent<Booton>().touchPosition = pos;
-                    recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
-                }
-            }
-            foreach (var g in touchesOld)
-            {
+                    if (Physics.Raycast(ray, out hit, touchInputMask))
+                    {
+                        GameObject recipient = hit.transform.gameObject;
+                        touchList.Add(recipient);
 
-                if (!touchList.Contains(g))
-                {
-                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                            Debug.Log("pressDown");
+                            dist = hit.distance; //hit.distance + Camera.main.nearClipPlane;
+                            trim = hit.collider.transform.position - hit.point;
+
+                            recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                            //recipient.GetComponent<Booton>().touchPosition = hit.point;
+                        }
+                        if (Input.GetMouseButtonUp(0))
+                        {
+                            recipient.SendMessage("OnTouchUP", hit.point, SendMessageOptions.DontRequireReceiver);
+                        }
+                        if (Input.GetMouseButton(0))
+                        {
+                            Debug.Log("mouseButton");
+                            Vector3 pos = Input.mousePosition;
+                            pos.z = dist;
+                            //pos = ray.origin + ray.direction * dist;
+                            //pos = ray.direction+ trim;
+                            pos = Camera.main.ScreenToWorldPoint(pos) + trim; // это новая координата
+
+                            recipient.GetComponent<Booton>().touchPosition = pos;
+                            recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                        }
+                    }
+                    foreach (var g in touchesOld)
+                    {
+
+                        if (!touchList.Contains(g))
+                        {
+                            g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                        }
+                    }
                 }
-            }
+                break;
+            case state.rotation:
+
+
+                break;
         }
 
 
 
-
 #endif
+
+
         if (Input.touchCount > 0)
         {
             touchesOld = new GameObject[touchList.Count];
@@ -158,21 +164,6 @@ public class TouchScript : MonoBehaviour
             }
 
         }
-                break;
-            case state.rotation:
-
-
-                break;
-
-        }
-
-    }
-
-    private void drawLine(Ray ray, Vector3 hit)
-    {
-        return;
-        var drawRender = GetComponent<LineRenderer>();
-        drawRender.SetPosition(0, ray.origin);
-        drawRender.SetPosition(1, hit);
     }
 }
+
